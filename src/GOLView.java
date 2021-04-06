b31faf26a07b1e141951542921892a01981bf57b
Cal Bigham <cbigham@monmouthcollege.edu>
2021-04-05 22:21:49 -0500

    Added a grid to the GUI, and some touches that I thought made it look nicer. Not Perfect, but progress.
    Signed-off-by: Cal Bigham <cbigham@monmouthcollege.edu>

--- src/GOLView.java
+++ src/GOLView.java
@@ -1,45 +1,78 @@
+
 import java.awt.*;
 import javax.swing.*;
-//import java.util.*;
 
 public class GOLView {
-        public GOLView(){
+        public GOLView() {
 // create objects (buttons, textfields, and speed slider
-                JFrame frame;
-                JPanel panel;
+                JFrame gameframe;
+                JPanel buttonpanel;
+                JPanel gamegrid;
+                JPanel mainpanel;
+                JPanel controlpanel;
+                JPanel sliderpanel;
                 JButton startstop;
                 JButton backward;
                 JButton forward;
                 JTextField turnnumber;
                 JSlider speed;
-                //private GameBoard gol_gameboard;
-                final int speed_min = 1;
+
+                final int boardWidth = 10;
+                final int boardLength = 10;
+
+                final int speed_min = 0;
                 final int speed_max = 10;
                 final int speed_init = 5;
 
-                frame = new JFrame();
-                frame.getContentPane().setLayout(new BorderLayout());
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setSize(500, 120);
-                frame.setLocationRelativeTo(null);
+                gameframe = new JFrame();
+                gameframe.getContentPane().setLayout(new BorderLayout());
+                gameframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                gameframe.setSize(700,500);
+                gameframe.setLocationRelativeTo(null);
 
                 // Create actual UI
                 startstop = new JButton("Start/Stop");
                 backward = new JButton("Look Back");
+                turnnumber = new JTextField("0");
                 forward = new JButton("Look Forward");
-                turnnumber = new JTextField();
                 speed = new JSlider(JSlider.HORIZONTAL, speed_min, speed_max, speed_init);
 
                 // Add these to a Panel at bottom of page
-                panel = new JPanel();
-                panel.add(startstop);
-                panel.add(backward);
-                panel.add(forward);
-                panel.add(turnnumber);
-                panel.add(speed);
+                FlowLayout buttonLayout = new FlowLayout();
+                controlpanel = new JPanel();
+                controlpanel.setLayout(new BoxLayout(controlpanel, BoxLayout.Y_AXIS));
+                sliderpanel = new JPanel();
+                buttonpanel = new JPanel();
+                buttonpanel.setLayout(buttonLayout);
+                buttonpanel.add(startstop);
+                buttonpanel.add(backward);
+                buttonpanel.add(turnnumber);
+                buttonpanel.add(forward);
+                sliderpanel.add(speed);
+                controlpanel.add(buttonpanel);
+                controlpanel.add(sliderpanel);
 
-                frame.add(panel);
-                frame.setVisible(true);
+                // Create game board
+                gamegrid = new JPanel();
+                JButton[][] gridButton = new JButton[boardWidth][boardLength];
+                gamegrid.setLayout(new GridLayout(boardWidth, boardLength, 0, 0));
+                for (int y = 0; y < boardLength; y++) {
+                        for (int x = 0; x < boardWidth; x++) {
+                                gridButton[x][y] = new JButton(x + ":" + y);
+                                gridButton[x][y].setActionCommand(x + ":" + y);
+                                gridButton[x][y].setText(" ");
+                                gamegrid.add(gridButton[x][y]);
+                        }
+                }
+
+
+                // Add User Buttons and grid
+                mainpanel = new JPanel();
+                mainpanel.setLayout(new BoxLayout(mainpanel, BoxLayout.Y_AXIS));
+                mainpanel.add(gamegrid);
+                mainpanel.add(controlpanel);
+                gameframe.add(mainpanel);
+                gameframe.setVisible(true);
         }
         public static void main(String[] args){
                 GOLView theview = new GOLView();
